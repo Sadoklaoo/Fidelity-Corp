f5eabdb52936b8628db205bde02c2fe70f1767bc
Sadoklaoo <47226412+Sadoklaoo@users.noreply.github.com>
2020-02-07 21:40:55 +0100

    Initial commit

---
+++ app/src/main/java/com/example/fidelitycorporation/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.example.fidelitycorporation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_login);
+    }
+}
